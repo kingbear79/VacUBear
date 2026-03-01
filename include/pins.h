dd2dab84d1e29fb41c9f4d402fcde4723db133a9
Christian Schweden <cschweden@gmail.com>
2026-03-01 06:35:32 +0100

    general Update

--- include/pins.h
+++ include/pins.h
@@ -4,8 +4,8 @@
 
 // Zentrale Pinbelegung fuer diese Hardwarevariante.
 // Bei neuen PCB-/Geraeteversionen nur hier anpassen.
-static const uint8_t PIN_PUMPE1 = D5;
+static const uint8_t PIN_PUMPE1 = D7;
 static const uint8_t PIN_PUMPE2 = D6;
-static const uint8_t PIN_VENTIL = D7;
+static const uint8_t PIN_VENTIL = D5;
 static const uint8_t PIN_TASTER = D2;
 static const uint8_t PIN_LED = D1;
